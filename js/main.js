0ded3fcc1aea4fa366da90e85ed37a282e0ab353
Rhyse <dev.rhyse@gmail.com>
2023-01-16 01:04:32 -0600

    set mars dataGrab

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
-document.querySelector('button').addEventListener('click', gatherNasaData())
+document.querySelector('#start').addEventListener('click', statGatheringData())
+
 class WeatherData{
-    constructor(localTemp,marsTemp){
-        this.local = localTemp,
-        this.mars = marsTemp
+    constructor(){
+        // this.local = localTemp,
+        this.marsMaxTemp = ""
+        this.marsMinTemp = ""
+        this.marsPressure = ""
     }
-    
-    
+    setMartianData(latestMax,latestMin,latestPressure){
+        this.marsMaxTemp = latestMax
+        this.marsMinTemp = latestMin
+        this.marspressure = latestPressure
+    }    
 }
-function gatherNasaData(){
-    fetch("https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0")
-        .then(res => res.json())
-        .then(data => console.log(data[sol].AT.at))
-        .catch(err => console.log(err))
+
+function statGatheringData(){
+    const talkingPoint = "https://api.maas2.apollorion.com/"
+    setMarsData(talkingPoint)
 }
+async function setMarsData(file){
+    let APIobject = await fetch(file)
+    if(!APIobject.ok){
+        throw new Error(console.log(APIobject.status))
+    }
+    let APIresult = await APIobject.json()
+    console.log(APIresult.max_temp)
+     await setMarsData(APIresult.max_temp,APIresult.min_temp,APIresult.pressure)
+}
+
+// https://maas2.apollorion.com/#/Latest/get_
+// used wesite for mars data
 
 // weather app idea
 
@@ -23,16 +40,17 @@ function gatherNasaData(){
 // get IP from user
 
 // if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-//     $ip = $_SERVER['HTTP_CLIENT_IP'];
+    //     $ip = $_SERVER['HTTP_CLIENT_IP'];
 // } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
 //     $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 // } else {
-//     $ip = $_SERVER['REMOTE_ADDR'];
-// }
-
-// rip from stackOverflow check for usability
-// https://www.abstractapi.com/guides/how-to-track-ip-address-of-website-visitors
-// extra reading
+    //     $ip = $_SERVER['REMOTE_ADDR'];
+    // }
+    
+    // rip from stackOverflow check for usability
+    // https://www.abstractapi.com/guides/how-to-track-ip-address-of-website-visitors
+    // extra reading
+    
 
 
 // Use gather IP to fetch weather data from NWS api
@@ -40,5 +58,4 @@ function gatherNasaData(){
 // show weather data to user
 
 
-// get mars data
 
