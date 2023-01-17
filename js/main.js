f1e812e78a8f489403b08db67950b498e416fde7
Rhyse <dev.rhyse@gmail.com>
2023-01-16 21:56:21 -0600

    set up gitingore and added earth data API fetch

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ class WeatherData{
         this.marsMinTemp = ''
         this.marsPressure = ''
     }
-    async fetchData() {
+    async fetchMarsData() {
         const talkingPoint = "https://api.maas2.apollorion.com/"
         let APIobject = await fetch(talkingPoint)
         if (APIobject.status !== 200) {
@@ -17,6 +17,19 @@ class WeatherData{
             let APIresult = await APIobject.json()
             console.log(APIresult)
             this.setMarsWeather(APIresult.max_temp,APIresult.min_temp,APIresult.pressure)
+        }
+    }
+    async fetchEarthData() {
+        const APIkey = '16d6fbb585496fa921ab85ad22947271'
+        const talkingPoint = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={APIkey}'
+
+        let APIobject = await fetch(talkingPoint)
+        if (APIobject.status !== 200) {
+            console.log(`Error: ${APIobject.status}`)
+        } else {
+            let APIresult = await APIobject.json()
+            console.log(APIresult)
+            
         }
     }
     setMarsWeather(latestMax,latestMin,latestPressure){
@@ -38,7 +51,8 @@ class WeatherData{
 
 document.querySelector('#start').addEventListener('click',() => {
     const weatherData = new WeatherData()
-    weatherData.fetchData()
+    weatherData.fetchMarsData()
+    weatherData.fetchEarthData()
 })
 
 // https://maas2.apollorion.com/#/Latest/get_
@@ -71,4 +85,16 @@ document.querySelector('#start').addEventListener('click',() => {
 // show weather data to user
 
 
+/*
+Caching: One popular caching solution for Node.js is Redis. It's an in-memory data structure store that can be used as a cache, message broker, and a database.
 
+Visualization: For visualizing the weather data, you can use JavaScript libraries such as Chart.js or D3.js. These libraries make it easy to create interactive and customizable charts and graphs.
+
+Alerting: For sending alerts to users, you could use a service such as Twilio to send SMS messages, or a service such as SendGrid to send email messages.
+
+Creating an API: Express.js is a popular framework for creating APIs in Node.js. It provides a simple and flexible way to handle routing and middleware, and it can be used in conjunction with other popular libraries such as Mongoose (for MongoDB) and Passport (for authentication)
+
+
+For the Map visualization feature, you can use mapping libraries like leaflet.js or OpenLayers.
+
+*/
